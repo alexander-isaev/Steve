9bee436574db1da96226ffba73e52d27f5e920c1
JATragedian <JATragedian@yandex.ru>
2019-04-16 08:57:39 +0300

    Threads and stuff added.

---
+++ src/com/javacore/Steve/db/Record.java
@@ -0,0 +1,16 @@
+package com.javacore.Steve.db;
+
+import java.util.List;
+
+public class Record {
+
+    // Record in one line of database.
+    // 12; Alexey; javaCourse
+    List<String> values;
+
+    public Record(List<String> values) {
+        this.values = values;
+    }
+
+
+}
